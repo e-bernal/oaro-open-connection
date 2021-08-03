a030338f2d433f13f0ff9c90c767600711a13b8e
Emmanuel Bernal <emmanuel.bernalv@agilethought.com>
2021-08-03 16:05:18 -0500

    https request

--- index.js
+++ index.js
@@ -20,14 +20,41 @@ let openConnection = async (event) => {
         }
     };
 
+    var formData = {
+        'userIp': '201.157.55.68',
+        'uniqid': 'agile001',
+        'callback': 'https://develop.d2338pcgca4grp.amplifyapp.com/test',
+        'tokenFrom': tokenFrom
+    }
+
     var response = 'Lambda function';
 
-    await request(options, function (error, resp) {
-        console.log('errot: ' + JSON.stringify(error));
-        console.log('resp: ' + JSON.stringify(resp.body));
-        response = 'Hola mundo';
-        console.log('uno');
-    });
+    var req_options = {
+        host: 'test.common.nodalblock.com',
+        port: '443',
+        path: '/dev/js/open-connection',
+        method: 'POST',
+        headers: {
+            'Api-Key': 'd5735a26-23c4-44f3-8d45-82d2f05f4277'
+        }
+    }
+
+    const req = https.request(req_options, (resp) => {
+        resp.setEncoding('utf8');
+        resp.on('data', (chunk) => {
+            console.log('Response: ' + chunk);
+        })
+    })
+
+    req.write(formData);
+    req.end();
+
+    // await request(options, function (error, resp) {
+    //     console.log('errot: ' + JSON.stringify(error));
+    //     console.log('resp: ' + JSON.stringify(resp.body));
+    //     response = 'Hola mundo';
+    //     console.log('uno');
+    // });
 
     console.log('dos')
 
