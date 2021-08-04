d0c8d84b56de2ec8030127d2ab575e5533e4fb06
Emmanuel Bernal <emmanuel.bernalv@agilethought.com>
2021-08-04 16:33:29 -0500

    lambda ok

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
-let openConnection = async (event) => {
-    
-    let tokenFrom = event.tokenFrom;
-    
-    console.log(tokenFrom);
-    
-    var request = require('request');
-    
-    var options = {
-        'method': 'POST',
-        'url': 'https://test.common.nodalblock.com/dev/js/open-connection',
-        'headers': {
-            'Api-Key': 'd5735a26-23c4-44f3-8d45-82d2f05f4277'
-        },
-        formData: {
-            'userIp': '201.157.55.68',
-            'uniqid': 'agile001',
-            'callback': 'https://develop.d2338pcgca4grp.amplifyapp.com/test',
-            'tokenFrom': tokenFrom
-        }
-    };
+const querystring = require('querystring');
+const https = require('https');
 
-    var formData = {
-        'userIp': '201.157.55.68',
-        'uniqid': 'agile001',
-        'callback': 'https://develop.d2338pcgca4grp.amplifyapp.com/test',
-        'tokenFrom': tokenFrom
-    }
-
-    var response = 'Lambda function';
-
-    var req_options = {
-        host: 'test.common.nodalblock.com',
-        port: '443',
-        path: '/dev/js/open-connection',
-        method: 'POST',
-        headers: {
-            'Api-Key': 'd5735a26-23c4-44f3-8d45-82d2f05f4277'
-        }
-    }
-
-    const req = https.request(req_options, (resp) => {
-        resp.setEncoding('utf8');
-        resp.on('data', (chunk) => {
-            console.log('Response: ' + chunk);
-        })
-    })
-
-    req.write(formData);
-    req.end();
-
-    // await request(options, function (error, resp) {
-    //     console.log('errot: ' + JSON.stringify(error));
-    //     console.log('resp: ' + JSON.stringify(resp.body));
-    //     response = 'Hola mundo';
-    //     console.log('uno');
-    // });
-
-    console.log('dos')
-
-    //return response;
+const formData = {
+    userIp: "201.157.55.68",
+    uniqid: "agile001",
+    callback: "https://develop.d2338pcgca4grp.amplifyapp.com/test"
 };
 
-exports.handler = openConnection;
+const options = {
+    hostname: 'test.common.nodalblock.com',
+    port: 443,
+    path: '/dev/js/open-connection',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Api-Key': 'd5735a26-23c4-44f3-8d45-82d2f05f4277'
+    }
+}
+
+const doRequest = (data) => new Promise((resolve, reject) => {
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`);
+        res.setEncoding('utf8');
+        var result = '';
+        res.on('data', (d) => {
+            result += d;
+            //process.stdout.write(d);
+        });
+        
+        res.on('end', function () {
+            console.log('result: ' + result);
+            resolve(JSON.parse(result));
+        });
+        
+        res.on('error', function (err) {
+            console.log(err);
+        })
+    });
+    
+    req.on('error', (e) => {
+        console.log(e)
+        reject(e.message);
+    });
+    
+    console.log(data);
+    
+    req.write(data);
+    
+    req.end();
+})
+
+exports.handler = async (event) => {
+    console.log('tokenFrom = ' + event.token)
+    
+    formData['tokenFrom'] = event.token;
+    
+    var response;
+    await doRequest(querystring.stringify(formData))
+    .then((result) => {
+        response = result;
+    })
+    .catch((err) => {
+        response = err;
+    })
+    
+    return response;
+};
